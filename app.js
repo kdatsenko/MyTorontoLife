b31e9f1447ccbe55a63deae4157b6ea57c4eb477
Chris Dobson <chris@penguinleaf.com>
2015-12-02 18:53:04 -0500

    eliminated all traces of handlebars...

--- app.js
+++ app.js
@@ -363,22 +363,13 @@ var routes = require('./routes/index');
 
 app.use(logger('dev'));
 
-// view engine setup
-/*var exphbs = require('express-handlebars');
-app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}));
-app.set('view engine', '.hbs');*/
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname + '/public')));
-app.set('views', __dirname + '/public');
-//app.engine('html', require('ejs').renderFile);
-//app.set('view engine', 'html');
 
 app.use(session({resave: true, saveUninitialized: true, secret: '25jh345hj34b7h8f', cookie: { maxAge: null}}));
 
