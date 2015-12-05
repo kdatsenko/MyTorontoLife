7cf91c51bf6237e894701ece0867f0fdfe7adba4
adambenn <adambenn22@gmail.com>
2015-12-04 22:35:42 -0500

    Revamped login page
    loginbar is now a button to profile page and to logout.

--- public/controllers/loginController.js
+++ public/controllers/loginController.js
@@ -119,30 +119,6 @@ var crudApp = angular.module('crudApp');
 
   	}
 
-  	var redirect = function(){
-  		console.log('dwdwdfwf');
-  		$scope.showLogin = false;
-   		$scope.showRegister = false;
-  		return $location.path('/'); // path not hash
-  	}
-
-  	$scope.logout = function(){
-  		$scope.state.is_logged = false;
-  		/*$.ajax({
-  			url: "/auth/logout",
-  			type: "GET",
-  			success: function(data){
-  				location.href = "index.html"
-  			}
-  		})*/
-
-  		$http.get('/auth/logout').success(function(data, status, headers, config) {
-          	redirect();
-          });
-
-  	}
-
-
 
   	$scope.login_form = function(){
    		$scope.showLogin = true;
@@ -178,9 +154,9 @@ var crudApp = angular.module('crudApp');
   		//$scope.login(); //Katie
   		$http.get('/auth/loggedInUser').success(function(data, status, headers, config) {
           	$scope.logged = data.logged
-          	if(data.user){
-  				$scope.username = data.user.username
-  			}
+          	if($scope.logged){
+                $location.path("/feed")
+            }
           });
 
 
