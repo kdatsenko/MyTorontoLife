29795d9c830276cb53de02708e5a26f11f51fc21
Chris Dobson <chris@penguinleaf.com>
2015-12-06 00:29:24 -0500

    get post json- because curly brackets are artsy

--- public/controllers/permalinkController.js
+++ public/controllers/permalinkController.js
@@ -7,20 +7,6 @@ crudApp.controller('permalinkController', function($scope, $location, $http, sha
     if(post){
       $scope.post = post.data;
     }
-    else {
-      $scope.post = {_id: "aaaa5",
-       username: "Chris" ,
-       short_text: 'hey',
-        userid: {
-         imageurl: "https://www.gravatar.com/avatar/89e0e971f58af7f776b880d41e2dde43?size=50" },
-       date_posted: 'Sun Nov 29 2015 14:59:13 GMT-0500 (Eastern Standard Time)',
-
-       interestname : 'Cooking',
-       groupname: 'Toronto',
-       averagerating: 3.5,
-        hashtags: ['great', 'cool', 'iheartmyTO']
-      }
-    }
   },
   function errorCallback(response, status, headers, config) {
     if(status == 403){
