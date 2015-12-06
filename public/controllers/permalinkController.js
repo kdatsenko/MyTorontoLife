6bf2cf6fc9468561b5b4ef37cd5b8008a85ea900
Chris Dobson <chris@penguinleaf.com>
2015-12-06 00:37:39 -0500

    apparently there are colons in the url...

--- public/controllers/permalinkController.js
+++ public/controllers/permalinkController.js
@@ -2,7 +2,14 @@ var crudApp = angular.module('crudApp');
 
 crudApp.controller('permalinkController', function($scope, $location, $http, sharedService) {
   $scope.post = {};
-  $http.get('/posts/post?id='+$location.$$path.split('/')[2]).then(function(post){
+  String.prototype.trimLeft = function(charlist) {
+    if (charlist === undefined)
+      charlist = "\s";
+
+    return this.replace(new RegExp("^[" + charlist + "]+"), "");
+  };
+  $scope.postId = $location.$$path.split('/')[2].trimLeft(':')
+  $http.get('/posts/post?id='+$scope.postId).then(function(post){
     console.log(post);
     if(post){
       $scope.post = post.data;
