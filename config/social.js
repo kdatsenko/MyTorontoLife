8d1ae20fc7f3cc100518c9a8262a452caad9b445
adambenn <adambenn22@gmail.com>
2015-12-06 22:23:25 -0500

    Changed github link

--- config/social.js
+++ config/social.js
@@ -6,7 +6,7 @@ module.exports = function () {
 	passport.use(new GithubStrategy({
 		clientID: "d5953bfed87434d96c7c",
 		clientSecret: "3872f06a99cf3ee667b0375ad610df2c4f8da583",
-		callbackURL: "/auth/github/callback",
+		callbackURL: "https://polar-reef-5864.herokuapp.com/auth/github/callback",
 		passReqToCallback: true
 	},
 	function(req, accessToken, refreshToken, profile, done){
