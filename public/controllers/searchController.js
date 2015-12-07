b6da315c548701a006564e1cb45edb01443752a1
Jim <jim423@live.cn>
2015-12-06 21:43:00 -0500

    Merged with Adam. A few changes on userList.html and postList.html

--- public/controllers/searchController.js
+++ public/controllers/searchController.js
@@ -6,39 +6,34 @@ var crudApp = angular.module('crudApp');
  * Controller for login page template.
  */
 crudApp.controller('searchController', function ($scope, $http)  {
-  $mainScope = angular.element(document.querySelector('[ng-controller="mainController"]')).scope();
-  $feedScope = angular.element(document.querySelector('[ng-controller="feedController"]')).scope();
+  $mainScope = function(){return angular.element(document.querySelector('[ng-controller="mainController"]')).scope();}
+  $feedScope = function(){return angular.element(document.querySelector('[ng-controller="feedController"]')).scope();}
   $scope.submitSearch = function(){
-
+    // doing search on textchange instead of submit
   }
   $scope.search_text = "";
   $scope.search_results = {};
-  var searchTimeout = null;
+  var last_search = "";
   $scope.$watch("search_text", function(){
-
-      if(searchTimeout){clearTimeout(searchTimeout)}
-      searchTimeout = setTimeout(function(){
-        searchTimeout = null;
         if($scope.search_text != ""){
-          $mainScope.state.is_searching = true;
-          $feedScope.search_tag = $scope.search_text;
-          $http.get('/posts/search?q='+$scope.search_text).success(function(data){
-              $feedScope.Posts = data.data;
-              for(var i in $feedScope.Posts){
-                $feedScope.Posts[i].userid.imgurl = $feedScope.Posts[i].imageurl;
+          $mainScope().state.is_searching = true;
+          $mainScope().state.is_group_page = false;
+          $feedScope().search_tag = $scope.search_text;
+          $http.get('/posts/search?q='+$scope.search_text).success(function(data, a, b, c){
+              last_search = c.url.split('=')[1];
+              if($scope.search_text == last_search){
+                $feedScope().Posts = data.data;
+                for(var i in $feedScope().Posts){
+                  $feedScope().Posts[i].userid.imgurl = $feedScope().Posts[i].imageurl;
+                }
+                setTimeout($feedScope().$apply, 50);
               }
           });
         }
-        else{
-        $mainScope.state.is_searching = false;
-        $http.get('/dashboard').success(function(data){
-            $feedScope.Posts = data;
-            for(var i in $feedScope.Posts){
-              $feedScope.Posts[i].userid.imgurl = $feedScope.Posts[i].imageurl;
-            }
-        });
+        else if(last_search != ""){
+          last_search = "";
+          $mainScope().getMainDashBoard();
       }
-      }, 250);
   });
 
 });
